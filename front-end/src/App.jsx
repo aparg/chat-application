77574f89cd82c76be27f3a377436be4029707930
aparg <apargtm@gmail.com>
2023-12-10 00:16:13 +0545

    feat: use axios to fetch auth tokens &  roles

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Register } from "./components/Register";
+import { AuthProvider } from "./context/AuthContext";
+import Login from "./components/Login";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <React.StrictMode>
       {/* <Register /> */}
-      <Login></Login>
-    </>
+      <AuthProvider>
+        <Login></Login>
+      </AuthProvider>
+    </React.StrictMode>
   );
 }
 
--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -1,12 +1,49 @@
+import { useRef, useState, useEffect, useContext } from "react";
+import axios from "../api/axios";
+import AuthContext from "../context/AuthContext";
 const Login = () => {
+  const { setAuth } = useContext(AuthContext);
   const nameRef = useRef(null);
+  const errRef = useRef(null);
   const [name, setName] = useState("");
   const [pwd, setPwd] = useState("");
-
+  const [success, setSuccess] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+  const LOGIN_URL = "/auth";
   useEffect(() => {
     nameRef.current.focus();
   }, []);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        LOGIN_URL,
+        JSON.stringify({ name, pwd }),
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      console.log(response?.data?.accessToken);
+      const roles = response?.data?.roles;
+      console.log(roles);
+      setName(""); //clear the form
+      setPwd("");
+      setSuccess(true);
+      setAuth({ user, pwd, roles, accessToken });
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg("No response from server");
+      } else if (err.response?.status === 400) {
+        setErrMsg("Missing username or password");
+      } else if (err.response?.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg("Login Failed");
+      }
+      errRef.current.focus();
+    }
+  };
   // useEffect(() => {
   //   setValidName(NAME_REGEX.test(name));
   // }, [name]);
@@ -16,10 +53,19 @@ const Login = () => {
   //   setPwdMatch(pwd === confirmPwd);
   // }, [pwd, confirmPwd]);
 
-  return (
+  return success ? (
+    <section>Success</section>
+  ) : (
     <section className="formDiv">
+      <section
+        className={errMsg ? "showInsructions" : "hide"}
+        aria-live="assertive"
+        ref={errRef}
+      >
+        {errMsg}
+      </section>
       <h1 className="formHeading">Login</h1>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <label className="formLabel" htmlFor="uname">
           Username:
         </label>
@@ -44,7 +90,10 @@ const Login = () => {
           onChange={(e) => setPwd(e.target.value)}
           required
         ></input>
+        <button className="submitBtn">Submit</button>
       </form>
     </section>
   );
 };
+
+export default Login;
