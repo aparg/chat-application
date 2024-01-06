f73d0176804619f1a8659526b7af0fb7f52de438
aparg <apargtm@gmail.com>
2024-01-06 22:57:24 +0545

    style: add friend card style fix

--- front-end/src/components/MainPage/LeftPane/AddFriends/AddFriendCard.jsx
+++ front-end/src/components/MainPage/LeftPane/AddFriends/AddFriendCard.jsx
@@ -1,23 +1,18 @@
 import profileImg from "../../../../assets/images/profile.jpg";
 import useReceiverName from "../../../../hooks/useReceiverName";
 const AddFriendCard = ({ name }) => {
-  const { receiverName, setReceiverName } = useReceiverName();
-
-  const handleClick = async () => {
-    setReceiverName(name);
-  };
   return (
     <div
-      className={`self-center flex items-center my-1.5 w-full rounded-xl p-10 md:p-2 h-20 hover:cursor-pointer items-center  justify-items-stretch ${
-        receiverName === name && "bg-white shadow-md"
+      className={`flex items-center my-1.5 w-full rounded-xl p-10 md:p-2 h-20 hover:cursor-pointer justify-center 
       }`}
-      onClick={handleClick}
     >
-      <img className="rounded-full w-1/6" src={profileImg}></img>
-      <div className="flex mx-3 items-center">
-        <div className="text-black font-bold text-2xl md:text-sm">{name}</div>
-        <button className="bg-gray h-3/6">Add Friend</button>
+      <img className="rounded-full w-1/6 basis-1/6" src={profileImg}></img>
+      {/* <div className="flex items-center justify-left "> */}
+      <div className="text-black font-bold text-2xl md:text-sm basis-2/6 w-full text-center">
+        {name}
       </div>
+      <button className="bg-gray basis-3/6 w-full">Add Friend</button>
+      {/* </div> */}
     </div>
   );
 };
--- front-end/src/components/MainPage/LeftPane/AddFriends/AddFriends.jsx
+++ front-end/src/components/MainPage/LeftPane/AddFriends/AddFriends.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import AddFriendCard from "./AddFriendCard";
-
+import { v4 as uuidv4 } from "uuid";
 export const AddFriends = () => {
   const friendReqArray = ["ram", "hari", "laxman"];
   return (
     <div className="overflow-auto basis-2/12">
       {friendReqArray.map((data) => (
-        <AddFriendCard name={data} />
+        <AddFriendCard name={data} key={uuidv4()} />
       ))}
     </div>
   );
